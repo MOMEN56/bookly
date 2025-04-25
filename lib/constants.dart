acc33a599056caefb11e82d50f1af9e63ebe2a0c
MOMEN56 <5momenalaa5@gmail.com>
2025-04-25 19:54:31 +0300

    navigate to home view

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/cupertino.dart';
 
 const KprimaryColor = Color(0xff100B20);
+const KTranstionDuration = Duration(milliseconds: 250);
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/slidingtext.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,9 +20,10 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 600));
-    slideAnimation = Tween<Offset>(begin: Offset(0, 5), end: Offset.zero).animate(animationController);
-    animationController.forward();
+    initSlidingTextAnimation();
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => HomeView(), transition: Transition.leftToRight, duration: KTranstionDuration);
+    });
   }
 
   @override
@@ -30,5 +35,11 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.stretch, children: [Image.asset(AssetsData.logo), const SizedBox(height: 4), SlidingText(slideAnimation: slideAnimation)]);
+  }
+
+  void initSlidingTextAnimation() {
+    animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 600));
+    slideAnimation = Tween<Offset>(begin: Offset(0, 5), end: Offset.zero).animate(animationController);
+    animationController.forward();
   }
 }
